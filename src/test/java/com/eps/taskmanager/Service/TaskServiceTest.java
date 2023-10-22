6acc58b9a16f7996f7e31b4b206d26339d03fb8d
Eps <rarimar4@gmail.com>
2023-10-22 20:23:12 +1100

    Adding test case for task service

---
+++ src/test/java/com/eps/taskmanager/Service/TaskServiceTest.java
@@ -0,0 +1,16 @@
+package com.eps.taskmanager.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceTest {
+
+    @Test
+    void shouldReturnPayloadResponseWhenTaskIsCreated() {
+
+
+        assertEquals(createdTask);
+    }
+
+}
